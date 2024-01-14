dac5d9ac792108efbebc100803eafbd5b74770c0
EltonVe <150631012+EltonVe@users.noreply.github.com>
2024-01-14 18:24:23 +0100

    Add files via upload
    final change

--- home.js
+++ home.js
@@ -7,3 +7,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }); });
 
 
+    var allowRedirect = false; 
+
+    function attemptLogin(event) {
+    
+        var email = document.getElementById('form3Example3').value;
+        var password = document.getElementById('form3Example4').value;
+    
+        // Check if both email and password are provided
+        if (email.trim() !== '') {
+            // Check if the password is at least 8 characters long
+            if (password.trim() !== '' && password.trim().length >= 8) {
+                allowRedirect = true; 
+            } else {
+                alert('Password must be at least 8 characters long');
+                allowRedirect = false; 
+            }
+        } else {
+            alert('Please enter both email and password');
+            allowRedirect = false; 
+        }
+    
+
+        if (allowRedirect) {
+            window.location.href = 'products.html'; 
+        }
+    }
+    
+   
+      
--- script.js
+++ script.js
@@ -2,10 +2,4 @@ document.getElementById('sendMessageButton').addEventListener('click', function(
     alert('Message was successfully sent!');
 });
 
- document.addEventListener('DOMContentLoaded', function () {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-
-   navbarToggler.addEventListener('click', function () {
-       navbarCollapse.classList.toggle('show');
-    }); });
+ 
