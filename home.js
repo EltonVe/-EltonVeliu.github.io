923628d64b2823b93a055256c732c21b8dac42dc
EltonVe <150631012+EltonVe@users.noreply.github.com>
2024-01-30 11:41:06 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -6,30 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
        navbarCollapse.classList.toggle('show');
     }); });
 
-
-    var allowRedirect = false; 
-
-    function attemptLogin(event) {
-        
+    function attemptLogin() {
+        // Get email and password values from the input fields
         var email = document.getElementById('form3Example3').value;
         var password = document.getElementById('form3Example4').value;
-        
-        // Check if both email and password are provided
-        if (email.trim() !== '') {
-            // Check if the password is at least 8 characters long
-            if (password.trim() !== '' && password.trim().length >= 8) {
-                allowRedirect = true; 
-            } else {
-                alert('Password must be at least 8 characters long');
-                allowRedirect = false; 
-            }
-        } else {
-            alert('Please enter both email and password');
-            allowRedirect = false; 
-        }
     
-
-        if (allowRedirect) {
-            window.location.href = 'products.html'; 
+        // Regular expression pattern for basic email validation
+        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+        // Check if both email and password are provided
+        if (email.trim() === '' || password.trim() === '') {
+            alert('Please enter both email and password');
+        } else if (password.length < 8) {
+            alert('Password must have at least 8 characters');
+        } else if (!emailPattern.test(email)) {
+            alert('Please enter a valid email address');
+        } else {
+            // Perform login logic here, for example, redirect to products.html
+            window.location.href = 'products.html';
         }
     }
+    
